55232793a15d91d4df71d8f0c0745a32769fa877
JakeSiewJK64 <global.strenuous055@passinbox.com>
2026-04-29 11:02:50 +0800

    remove cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tsConfigPaths(),
     tanstackStart(),
     // react's vite plugin must come after start's vite plugin
